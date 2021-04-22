ed714fc7930e583ef91117fa76b5e7dbb7136041
Bedirhan Şen <45562388+bdrhnsen@users.noreply.github.com>
2021-04-22 10:50:35 +0300

    elapsed time added

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 void swap(int *a,int *b){
     int temp=*a;
@@ -28,17 +29,27 @@ void printArray(int arr[], int size)
     cout << endl;
 }
 int main() {
+    srand(time(0));
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+
+    start = std::chrono::system_clock::now();
+
     std::cout << "Hello, World!" << std::endl;
-    int array_size = rand()%100+1;
+    int array_size = 100000;
 
     int arr[array_size];
     for (int l;l<array_size;l++){
-       arr[l]=rand()%50+1;
-        cout<<arr[l];
+        arr[l]=rand()+1; //create random integers and fill the array with them
+
     }
     SelectionSort(arr,array_size);
+    end = std::chrono::system_clock::now();
     cout << "Sorting the array... \n" ;
     printArray(arr,array_size);
 
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
     return 0;
 }
